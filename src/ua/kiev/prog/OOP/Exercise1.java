44341fd985abfb22acb10232645608f44b69ecbc
funtik13 <funtik1990@bigmir.net>
2016-11-11 15:45:33 +0200

    HommeWorke

---
+++ src/ua/kiev/prog/OOP/Exercise1.java
@@ -0,0 +1,12 @@
+package ua.kiev.prog.OOP;
+
+
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(1, -1);
+        System.out.println(r.getS());
+
+    }
+}
